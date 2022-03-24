1ec9f4b18d71c2c8123dad809a88395405df1fe0
storres20 <sanchotorres777@gmail.com>
2022-03-24 02:22:35 -0500

    add projects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />

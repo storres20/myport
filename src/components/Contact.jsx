947fdbdf49b34d15543d1ba6ba851196500d36ba
storres20 <sanchotorres777@gmail.com>
2022-03-31 01:22:55 -0500

    add required to input data

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -87,6 +87,7 @@ export default function Contact() {
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={(e) => setName(e.target.value)}
+              required
             />
           </div>
           <div className="relative mb-4">
@@ -99,6 +100,7 @@ export default function Contact() {
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </div>
           <div className="relative mb-4">
@@ -112,6 +114,7 @@ export default function Contact() {
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               onChange={(e) => setMessage(e.target.value)}
+              required
             />
           </div>
           <button

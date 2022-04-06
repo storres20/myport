ceae0aab78fa33a29591f44126b8c38a2eae8360
storres20 <sanchotorres777@gmail.com>
2022-04-05 22:31:41 -0500

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a href="/#" className="title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl">
-            Reed Barger
+            MyPort
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
@@ -17,9 +17,9 @@ export default function Navbar() {
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
+          {/* <a href="#testimonials" className="mr-5 hover:text-white">
             Testimonials
-          </a>
+          </a> */}
         </nav>
         <a
           href="#contact"

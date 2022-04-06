6d8c7b14c39ccc6c39a74e9968afbe3a4e164cf5
storres20 <sanchotorres777@gmail.com>
2022-04-05 23:12:38 -0500

    fix Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-0 flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"

8a5f621f60ee3c01ec30b929ac69f9ec13f32ae6
storres20 <sanchotorres777@gmail.com>
2022-04-05 22:32:31 -0500

    update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,9 +14,7 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+            My skills and technologies to make amazing web apps.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">

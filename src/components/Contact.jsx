6e17e478cddf90bc232f6e41d6c4451aaa188e30
Italo Lon kan <sanchotorres777@gmail.com>
2023-04-20 12:39:18 -0500

    Update contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,15 @@
 // src/components/Contact.js
 
 import React from "react";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
 
 export default function Contact() {
 
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-0 flex items-end justify-start relative">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
@@ -16,28 +18,59 @@ export default function Contact() {
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49195.44575542484!2d-77.07755079077705!3d-12.045569372657702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c5f619ee3ec7%3A0x14206cb9cc452e4a!2sLima!5e0!3m2!1ses-419!2spe!4v1649216566412!5m2!1ses-419!2spe"
           />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-3/4 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                Lima <br />
+                PERU
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                EMAIL
+              </h2>
+              <p className="text-indigo-400 leading-relaxed">
+                lkpitalo249@email.com
+              </p>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                GITHUB
+              </h2>
+              <a href="https://github.com/storres20" target="_blank" rel="noreferrer">
+                <FiGithub className="h-8 w-8 text-gray-400 hover:text-violet-600" />
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                LINKEDIN
+              </h2>
+              <a href="https://www.linkedin.com/in/italo-lon-kan/" target="_blank" rel="noreferrer">
+                <FaLinkedinIn className="h-8 w-8 text-gray-400 hover:text-violet-600" />
+              </a>
+              
+            </div>
+          </div>
         </div>
         <form
-          data-netlify="true"
+          action="https://formspree.io/f/xyyvwalr" method="POST"
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
           <p className="leading-relaxed mb-5">
-            For any help or support with any project. Just, send me a message.
+            Feel free for sending me a message. Any idea in mind? Let's talk!
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
             </label>
             <input
-              value="ITALO"
-              disabled={true}
               type="text"
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
             />
           </div>
           <div className="relative mb-4">
@@ -45,46 +78,31 @@ export default function Contact() {
               Email
             </label>
             <input
-              value="lkpitalo249@gmail.com"
-              disabled={true}
               type="email"
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
             />
           </div>
-          
           <div className="relative mb-4">
-            <label htmlFor="github" className="leading-7 text-sm text-gray-400">
-              Github
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              Message
             </label>
-            <a href="https://github.com/storres20" target="_blank" rel="noreferrer">
-              <p className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out hover:bg-violet-600">https://github.com/storres20</p>
-            </a>
-          </div>
-          
-          <div className="relative mb-4">
-            <label htmlFor="linkedin" className="leading-7 text-sm text-gray-400">
-              LinkedIn
-            </label>
-            <a href="https://www.linkedin.com/in/italo-lon-kan/" target="_blank" rel="noreferrer">
-              <p className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out hover:bg-violet-600">https://www.linkedin.com/in/italo-lon-kan/</p>
-            </a>
-          </div>
-          
-          <div className="relative mb-4">
-            <label htmlFor="address" className="leading-7 text-sm text-gray-400">
-              Address
-            </label>
-            <input
-              value="Lima, Peru"
-              disabled={true}
-              type="text"
-              id="address"
-              name="address"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              required
             />
           </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
         </form>
       </div>
     </section>

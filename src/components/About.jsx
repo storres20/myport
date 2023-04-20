544f45609392d2572ae9cfa3cebcc09a19ec9f8e
Italo Lon kan <sanchotorres777@gmail.com>
2023-04-20 12:39:11 -0500

    Update about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,13 +10,22 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hello!!!
+            Welcome!!
           </h1>
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Welcome to my portfolio.
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            I'm a Frontend Developer with experience making web apps. 
+          <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            I'm ITALO LON KAN PEREZ
+          </h2>
+          <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Full Stack Web Developer
+          </h2>
+          <p className="mb-2 leading-relaxed md:text-2xl">
+            Expert in REACT JS, Ruby On Rails, and more.
+          </p>
+          <p className="mb-8 leading-relaxed md:text-2xl">
+            Completely immersed in remote software development for the last few months, I have been working remotely and asynchronously with people in different time zones, and have built many projects.
+          </p>
+          <p className="mb-8 leading-relaxed md:text-2xl">
+            Any idea in mind? Click on "Work With Me" to send me a message. Let's talk!
           </p>
           <div className="flex justify-center">
             <a
@@ -33,7 +42,7 @@ export default function About() {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            className="object-cover object-center rounded h-80"
+            className="object-cover object-center rounded h-full rounded-lg"
             alt="hero"
             src={logo}
           />

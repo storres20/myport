62b90c6cc53a242017a24a56955235f84cec7b8b
Italo Lon kan <sanchotorres777@gmail.com>
2023-04-20 21:51:04 -0500

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a href="/#" className="title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl">
-            MyPort
+            LON KAN
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">

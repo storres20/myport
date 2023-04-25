65cb0ecc452b7d4db45564347d122b87184320d7
storres20 <sanchotorres777@gmail.com>
2023-04-25 16:44:37 -0500

    Fix code reviewer's suggestion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,7 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              className="inline-flex text-black bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
               Work With Me
             </a>
             <a
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -100,7 +100,7 @@ export default function Contact() {
           </div>
           <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            className="text-white bg-indigo-900 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
         </form>

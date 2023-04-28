4bb314d0abcf32b191ddf19faa1d904fcf7d35cc
storres20 <sanchotorres777@gmail.com>
2023-04-28 12:38:10 -0500

    Update navbar margin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href="/#" className="title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl">
+        <a href="/#" className="title-font font-medium text-white mb-4 md:mb-0 md:ml-3 text-xl">
             LON KAN
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center gap-4">
+          <a href="#projects" className="hover:text-white">
             Past Work
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="hover:text-white">
             Skills
           </a>
           {/* <a href="#testimonials" className="mr-5 hover:text-white">

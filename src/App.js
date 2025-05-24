13348f63837cbe3f30f606ad6caab2c72deb7350
storres20 <sanchotorres777@gmail.com>
2025-05-23 19:43:10 -0500

    Update main file to include interaction with Privacy page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch, useLocation } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import ScrollButton from "./components/ScrollButton";
 import Skills from "./components/Skills";
-//import Testimonials from "./components/Testimonials";
+import Privacy from "./components/Privacy";
 
-function App() {
+function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollToId) {
+      const el = document.getElementById(location.state.scrollToId);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
+    <>
       <About />
       <Projects />
       <Skills />
-      {/* <Testimonials /> */}
       <Contact />
-      {/* Scroll button for going back to TOP page */}
       <ScrollButton />
-    </main>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/privacidad" component={Privacy} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 

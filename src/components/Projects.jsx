d731107049e9f6024ba97d28ec61609715ff5e29
storres20 <sanchotorres777@gmail.com>
2025-06-26 17:36:24 -0500

    Update projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -39,7 +39,7 @@ export default function Projects() {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={require('../' + project.image + '.png')}
+                  src={require('../' + project.image + '.gif')}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
